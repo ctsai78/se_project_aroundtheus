8f4b9bc1c5b078db901b1ca7ffdb8c54479f1acf
Chang Min Tsai <changt@Changs-MacBook-Air.local>
2023-07-03 01:30:05 -0400

    project 8 for tutor

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -7,12 +7,12 @@ export default class Popup {
 
   open() {
     this._popupElement.classList.add("modal_opened");
-    document.addEventListener("keydown", this._handleEscClose());
+    document.addEventListener("keydown", this.handleEscClose());
   }
 
   close() {
     this._popupElement.classList.remove("modal_opened");
-    document.removeEventListener("keydown", this._handleEscClose());
+    document.removeEventListener("keydown", this.handleEscClose());
   }
 
   handleEscClose(event) {
